1ffc1d1bc6d792cec426a8f637b2b428c42ffa46
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-24 09:20:35 +0300

    Proje dosyası ekle.

---
+++ DoWhile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // *** 1-10 arası sayilari dowhile dongusu kullanarak konsola yazdiralim ; ***
+            int sayi;
+            sayi = 1;
+
+            do
+            {
+                Console.WriteLine(sayi);
+                sayi++;
+            }
+            while (sayi <= 10);
+
+
+            Console.Read();
+        }
+    }
+}
